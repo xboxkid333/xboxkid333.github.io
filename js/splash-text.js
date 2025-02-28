2630f732d41c1eb97efedc06e616670064479650
Do_Not_Dusturb <152636544+Do-Not-Dusturb@users.noreply.github.com>
2025-02-28 08:39:22 -0500

    Create splash-text.js

---
+++ js/splash-text.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Array 
+  var splashTexts = [
+    "Hit q for panic button",
+    "never leave the site",
+    "Thanks for using this btw",
+    "do our google form",
+    "Never play games in shcool, it is very bad"
+  ];
+
+  // random text
+  function displaySplashText() {
+    const splashElement = document.querySelector(".splash-text");
+    if (splashElement) {
+      const randomIndex = Math.floor(Math.random() * splashTexts.length);
+      splashElement.textContent = splashTexts[randomIndex];
+    }
+  }
+
+  // Call the function i hope
+  displaySplashText();
+});
